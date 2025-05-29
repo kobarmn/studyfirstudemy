fcc29efc2d12ce2fb7bf653b2a45f6907858cf7a
kobarmn <kout.kobayashi@gmail.com>
2025-05-29 22:54:47 +0900

    BookMarkApp 3

--- lib/main.dart
+++ lib/main.dart
@@ -30,27 +30,70 @@ class _BookMarkMainPage extends StatefulWidget {
 }
 
 class _BookMarkMainPageState extends State<_BookMarkMainPage> {
+  List<List<String>> items = [
+    ['Flutter','https://flutter.dev/' ],
+    ['Google','https://www.google.co.jp/' ],
+    ['Youtube','https://www.youtube.com/' ],
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ブックマーク'),
       ),
-      body: Center(
-        child: ListView(
-          children: [
-            ListTile(
-              title: Text('Flutter'),
-            ),
-            ListTile(
-              title: Text('Google'),
-            ),
-            ListTile(
-              title: Text('Youtube'),
-            )
-          ],
-        ),
+      body: ListView.builder (
+        itemBuilder: (context, index) {
+          final item = items[index];
+          final title = item[0] ?? '';
+          final url = item[1] ?? '';
+          return ListTile(
+            title: Text(title),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return WebViewPage(title: title, url: url);
+              }));
+            },
+          );
+        },
+          itemCount: items.length
       ),
     );
   }
 }
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key, required this.title, required this.url});
+  final String title;
+  final String url;
+
+  @override
+  State<WebViewPage> createState() => WebViewPageState();
+}
+
+class WebViewPageState extends State<WebViewPage> {
+  late final WebViewController controller;
+  
+  @override
+  void initState(){
+    super.initState();
+    controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(
+      Uri.parse(widget.url),
+    );
+  }
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
+
